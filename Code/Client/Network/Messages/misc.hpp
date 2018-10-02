d1cd8185478eedead0f89003a7e2d66d4c82c663
Dominik Madarász <zaklaus@outlook.com>
2018-10-03 01:05:03 +0200

    Fixed buffer overflow

--- Code/Client/Network/Messages/misc.hpp
+++ Code/Client/Network/Messages/misc.hpp
@@ -1,9 +1,11 @@
 librg_network_add(&network_context, NETWORK_SEND_CHAT_MSG, [](librg_message_t* msg) {
-    char chat_line[128] = "";
-
     auto chat_len = librg_data_ru8(msg->data);
+
+    zpl_string chat_line = zpl_string_make_reserve(zpl_heap(), chat_len);
     librg_data_rptr(msg->data, chat_line, chat_len);
     chat::chat_messages.push_back(std::make_pair(ImVec4(1.0 / 250.0 * 150, 1.0f, 1.0f, 1.0f), chat_line));
+
+    zpl_string_free(chat_line);
 });
 
 librg_network_add(&network_context, NETWORK_SEND_FADEOUT, [](librg_message_t* msg) {
