d3259389ce2bcc12be5a98f20f1e34db634e2bff
DavoSK <david.svitana@gmail.com>
2018-11-22 00:08:33 +0100

    Added reversed function for dropping components, and explosion

--- Vendors/mafia-sdk/MafiaSDK/Game/C_Car.hpp
+++ Vendors/mafia-sdk/MafiaSDK/Game/C_Car.hpp
@@ -1,17 +1,17 @@
 /*
-	Copyright 2017 D�vid Svitana
+Copyright 2017 D�vid Svitana
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 #ifndef _CCAR_H_
@@ -35,24 +35,25 @@ namespace MafiaSDK
 			Engine = 0x004E1CE0,
 			SetEngineOn = 0x004CB5B0,
 			SetGear = 0x004CB070,
-			SetPower = 0x004CB130,
-			MotorForce = 0x004E15B0,
 			GearSnd = 0x004ED810,
 			GetSeatProperty = 0x0041DC30,
 			GetOwner = 0x0041DEC0,
 			Update = 0x0041FAC0,
+			PrepareDropOutWheel = 0x00426DD0,
+			PrepareDropOut = 0x00426EC0,
+			CarExplosion = 0x00421D60
 		};
 	};
 
 	class C_Car : public C_Actor
 	{
 	public:
-		C_Car_Interface* GetInterface()
+		C_Car_Interface * GetInterface()
 		{
 			return reinterpret_cast<C_Car_Interface*>(this);
 		}
 
-		void Update(float dt) 
+		void Update(float dt)
 		{
 			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::Update;
 
@@ -70,8 +71,8 @@ namespace MafiaSDK
 			{
 				mov ecx, this
 				mov eax, dword ptr ds : [ecx]
-				push dt				
-				call dword ptr ds :[eax + 0x34]
+				push dt
+				call dword ptr ds : [eax + 0x34]
 			}
 		}
 
@@ -90,9 +91,9 @@ namespace MafiaSDK
 				call functionAddress
 			}
 		}
-		
+
 		/* --- C_Vehicle Function here :) --- */
-		void SetEngineOn(BOOL arg1, BOOL arg2) 
+		void SetEngineOn(BOOL arg1, BOOL arg2)
 		{
 			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::SetEngineOn;
 			__asm
@@ -106,7 +107,7 @@ namespace MafiaSDK
 			}
 		}
 
-		void SetGear(int gear) 
+		void SetGear(int gear)
 		{
 			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::SetGear;
 			__asm
@@ -118,33 +119,7 @@ namespace MafiaSDK
 			}
 		}
 
-		void MotorForce(float unk1, float unk2)
-		{
-			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::MotorForce;
-			__asm
-			{
-				mov ecx, this
-				add ecx, 0x70
-				push unk2
-				push unk1
-				call functionAddress
-			}
-		}
-
-		void SetPower(float power, BOOL unk)
-		{
-			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::SetPower;
-			__asm
-			{
-				mov ecx, this
-				add ecx, 0x70
-				push power
-				push unk
-				call functionAddress
-			}
-		}
-
-		void GearSnd() 
+		void GearSnd()
 		{
 			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::GearSnd;
 			__asm
@@ -172,8 +147,8 @@ namespace MafiaSDK
 			}
 		}
 
-		/* Just our custom function why not*/ 
-		void UpdatePos(const Vector3D & pos, const Vector3D & rot) 
+		/* Just our custom function why not*/
+		void UpdatePos(const Vector3D & pos, const Vector3D & rot)
 		{
 			//Get state of actor and if actor is in sleeping mode or non active 
 			//We make the actor as an active one
@@ -194,6 +169,43 @@ namespace MafiaSDK
 			__asm
 			{
 				push seat_idx
+				mov ecx, this
+				call functionAddress
+			}
+		}
+
+		void Prepare_Drop_Out_Wheel(unsigned int wheel_idx, const Vector3D & speed, const Vector3D & unk)
+		{
+			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::PrepareDropOutWheel;
+
+			__asm {
+				push unk
+				push speed
+				mov edx, wheel_idx
+				mov ecx, this
+				call functionAddress
+			}
+		}
+
+		void Prepare_Drop_Out(unsigned int comp_idx, const Vector3D & speed, const Vector3D & unk)
+		{
+			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::PrepareDropOut;
+
+			__asm {
+				push unk
+				push speed
+				mov edx, comp_idx
+				mov ecx, this
+				call functionAddress
+			}
+		}
+
+		void CarExplosion(unsigned int tick_time)
+		{
+			unsigned long functionAddress = C_Car_Enum::FunctionsAddresses::CarExplosion;
+
+			__asm {
+				push tick_time
 				mov ecx, this
 				call functionAddress
 			}
