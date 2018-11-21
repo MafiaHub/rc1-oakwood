a026430252c1f0cade155ca93e41a2dbea51062b
LordCukier <lordcukier@gmail.com>
2018-11-21 22:01:49 +0100

    Beautify the health bar

--- Code/Client/Graphics/Elements/nameplates.hpp
+++ Code/Client/Graphics/Elements/nameplates.hpp
@@ -15,6 +15,15 @@ namespace nameplates {
 		}
 	}
 
+	DWORD mod_vec3_to_color(zpl_vec3 rhs) {
+		DWORD color = 0x0FF000000;
+		color |= ((DWORD)rhs.r << 16);
+		color |= ((DWORD)rhs.g << 8);
+		color |= ((DWORD)rhs.b);
+
+		return color;
+	}
+
     void render() {
         
         if(!voip_texture) {
@@ -26,7 +35,7 @@ namespace nameplates {
         for (u32 i = 0; i < network_context.max_entities; i++) {
 
             librg_entity *entity = librg_entity_fetch(&network_context, i);
-            if (!entity || entity->id == local_player.entity.id) continue;
+			if (!entity || entity->id == local_player.entity.id) continue;
 
             if (entity->type == TYPE_PLAYER && entity->user_data) {
                 auto player = reinterpret_cast<mafia_player*>(entity->user_data);
@@ -34,7 +43,6 @@ namespace nameplates {
                     auto player_pos = player->ped->GetInterface()->neckFrame->GetInterface()->mPosition;
 					auto player_health = player->ped->GetInterface()->health;
 					
-					bool in_same_car = false;
 					Vector3D camera_pos = get_current_i3dcamera()->GetInterface()->mPosition;
 
                     {
@@ -42,9 +50,6 @@ namespace nameplates {
 
                         if (!local_player_int)
                             break;
-
-                        //local_player_pos = local_player_int->GetInterface()->humanObject.neckFrame->GetInterface()->mPosition;
-						in_same_car = (local_player_int->GetInterface()->humanObject.playersCar == player->ped->GetInterface()->playersCar) && player->ped->GetInterface()->playersCar != nullptr;
 					}
 
                     auto screen = world_to_screen({ player_pos.x, player_pos.y + 0.45f, player_pos.z });
@@ -84,8 +89,20 @@ namespace nameplates {
 
 						screen.y += scaled_height + 5.0f;
 						render_x = screen.x - (scaled_width / 2);
-						MafiaSDK::GetMission()->GetGame()->GetIndicators()->FilledRectangle(render_x, screen.y, scaled_width, scaled_height, 0x0FFFF0000, player_health / 200.0f);
-						MafiaSDK::GetMission()->GetGame()->GetIndicators()->Rectangle(render_x - 1.0f, screen.y - 1.0f, scaled_width + 1.0f, scaled_height + 1.0f, 0xFFFFFFFF);
+
+						zpl_vec3 health_color_min = { 255.0f, 0.0f, 0.0f };
+						zpl_vec3 health_color_max = { 0.0f, 255.0f, 0.0f };
+						zpl_vec3 calculated_color, background_color;
+						f32 health = zpl_clamp(player_health / 200.0f, 0.0f, 1.0f);
+						zpl_vec3_lerp(&calculated_color, health_color_min, health_color_max, health);
+						zpl_vec3_mul(&background_color, calculated_color, 0.2f);
+
+						DWORD final_color = mod_vec3_to_color(calculated_color);
+						DWORD final_bg_color = mod_vec3_to_color(background_color);
+
+
+						MafiaSDK::GetMission()->GetGame()->GetIndicators()->FilledRectangle(render_x - 2.0f, screen.y - 2.0f, scaled_width + 2.0f, scaled_height + 2.0f, final_bg_color, 1.0f);
+						MafiaSDK::GetMission()->GetGame()->GetIndicators()->FilledRectangle(render_x, screen.y, scaled_width, scaled_height, final_color, health);
                     }
                 }
             }
