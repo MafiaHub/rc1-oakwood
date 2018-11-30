2f37c6f8e4a9cd57574eb306176e9aa728e5e457
DavoSK <david.svitana@gmail.com>
2018-12-01 00:30:23 +0100

    Refactored graphics.hpp added some usefull methods + DirectX renderer for nameplates

--- Code/Client/Graphics/Elements/nameplates.hpp
+++ Code/Client/Graphics/Elements/nameplates.hpp
@@ -1,13 +1,13 @@
 #pragma once
-
 /* 
 * Renders simple nicknames 
 * TODO(DavoSK): Render to texture with antialasing to dont see nicknames behind walls
 */
 namespace nameplates {
-    MafiaSDK::ITexture* voip_texture = nullptr;
+   
+    ID3DXFont* nameplate_font = nullptr;
 
-    MafiaSDK::I3D_Frame* get_current_i3dcamera() {
+    inline auto get_current_i3dcamera() -> MafiaSDK::I3D_Frame* {
         __asm {
             mov eax, dword ptr ds : [63788Ch]
             mov ecx, dword ptr ds : [eax + 10h]
@@ -15,98 +15,113 @@ namespace nameplates {
         }
     }
 
-    DWORD mod_vec3_to_color(zpl_vec3 rhs) {
+    inline auto mod_vec3_to_color(zpl_vec3 rhs) {
         DWORD color = 0x0FF000000;
         color |= ((DWORD)rhs.r << 16);
         color |= ((DWORD)rhs.g << 8);
         color |= ((DWORD)rhs.b);
-
         return color;
     }
 
-    
-    void render() {
-        
-        if(!voip_texture) {
-            voip_texture = MafiaSDK::GetIGraph()->CreateITexture();
-            voip_texture->Create("gramafon.tga", "", 8, 100, 100, 0);
-        }
-        
-
+    inline auto iterate_players(std::function<void(mafia_player*)> fptr) {
         for (u32 i = 0; i < network_context.max_entities; i++) {
 
             librg_entity *entity = librg_entity_fetch(&network_context, i);
-            if (!entity || entity->id == local_player.entity.id) continue;
+            if (entity == nullptr || entity->id == local_player.entity.id) continue;
 
-            if (entity->type == TYPE_PLAYER && entity->user_data) {
-                auto player = reinterpret_cast<mafia_player*>(entity->user_data);
-                if (player->ped) {
-                    auto player_pos = player->ped->GetInterface()->neckFrame->GetInterface()->mPosition;
-                    auto player_health = player->health;
-                    
-                    S_vector camera_pos = get_current_i3dcamera()->GetInterface()->mPosition;
-
-                    {
-                        auto local_player_int = local_player.ped;
-
-                        if (!local_player_int)
-                            break;
-                    }
-
-                    auto screen = world_to_screen({ player_pos.x, player_pos.y + 0.45f, player_pos.z });
-                    
-                    if(screen.z < 1.0f) {
-                        if(zpl_time_now() - player->last_talked < 5.0f && voip_texture)
-                            voip_texture->Draw2D(screen.x - (100 / 2),  screen.y - 150);
-
-                        zpl_vec3 vec = {};
-                        zpl_vec3_sub(&vec, EXPAND_VEC(player_pos), EXPAND_VEC(camera_pos));
-                        auto dist = zpl_vec3_mag(vec);
-                        auto dist_sq = zpl_sqrt(dist);
-
-
-                        constexpr auto orig_font_size = 20.0f;
-                        auto font_size = orig_font_size / dist_sq;
-                        if (font_size > orig_font_size)
-                            font_size = orig_font_size;
-
-                        auto size = MafiaSDK::GetMission()->GetGame()->GetIndicators()->TextSize(player->name, font_size, 1, 0);
-                        auto render_x = screen.x - (size / 2);
-                        auto render_y = screen.y;
-                        
-                        MafiaSDK::GetMission()->GetGame()->GetIndicators()->OutText(player->name, screen.x - (size / 2), screen.y, size, font_size, 0xFFFFFFFF, 0, 1);
-                        
-                        constexpr auto health_width = 135.0f;
-                        constexpr auto health_height = 15.0f;
-
-                        auto scaled_width = health_width / dist_sq;
-                        auto scaled_height = health_height / dist_sq;
-
-                        if (scaled_width > health_width)
-                            scaled_width = health_width;
-                        
-                        if (scaled_height > health_height)
-                            scaled_height = health_height;
-
-                        screen.y += scaled_height + 5.0f;
-                        render_x = screen.x - (scaled_width / 2);
-
-                        zpl_vec3 health_color_min = { 255.0f, 0.0f, 0.0f };
-                        zpl_vec3 health_color_max = { 0.0f, 255.0f, 0.0f };
-                        zpl_vec3 calculated_color, background_color;
-                        f32 health = zpl_clamp(player_health / 200.0f, 0.0f, 1.0f);
-                        zpl_vec3_lerp(&calculated_color, health_color_min, health_color_max, health);
-                        zpl_vec3_mul(&background_color, calculated_color, 0.2f);
-
-                        DWORD final_color = mod_vec3_to_color(calculated_color);
-                        DWORD final_bg_color = mod_vec3_to_color(background_color);
-
-
-                        MafiaSDK::GetMission()->GetGame()->GetIndicators()->FilledRectangle(render_x - 2.0f, screen.y - 2.0f, scaled_width + 2.0f, scaled_height + 2.0f, final_bg_color, 1.0f);
-                        MafiaSDK::GetMission()->GetGame()->GetIndicators()->FilledRectangle(render_x, screen.y, scaled_width, scaled_height, final_color, health);
-                    }
-                }
+            if (entity->type == TYPE_PLAYER && entity->user_data && fptr != nullptr) {
+                fptr(reinterpret_cast<mafia_player*>(entity->user_data));
             }
         }
+    }
+
+    inline void init(IDirect3DDevice9* device) {
+        nameplate_font = graphics::create_font(device, "tahoma-bold", 22, true);
+    }
+
+    /*
+    * When device lost 
+    * Free all needed elements
+    */
+    inline void device_lost() {
+        if (nameplate_font) {
+            nameplate_font->Release();
+            nameplate_font = nullptr;
+        }
+
+        if (nameplate_font) {
+            nameplate_font->Release();
+            nameplate_font = nullptr;
+        }
+    }
+
+    /* 
+    * Recreated all freed elements after device is reseted
+    */
+    inline void device_reset(IDirect3DDevice9* device) {
+        init(device);
+    }
+
+    inline void render(IDirect3DDevice9* device) {
+
+        iterate_players([=](mafia_player* player) {
+            if (player->ped && player->ped->GetInterface()->neckFrame) {
+                auto player_pos = player->ped->GetInterface()->neckFrame->GetInterface()->mPosition;
+                auto player_health = player->health;
+
+                auto current_i3d_camera = get_current_i3dcamera();
+                if (current_i3d_camera == nullptr) return;
+
+                S_vector camera_pos = current_i3d_camera->GetInterface()->mPosition;
+                auto screen = world_to_screen({ player_pos.x, player_pos.y + 0.45f, player_pos.z });
+
+                if (screen.z < 1.0f) {
+
+                    zpl_vec3 vec = {};
+                    zpl_vec3_sub(&vec, EXPAND_VEC(player_pos), EXPAND_VEC(camera_pos));
+                    auto dist = zpl_vec3_mag(vec);
+                    auto dist_sq = zpl_sqrt(dist);
+
+                    constexpr auto orig_font_size = 30.0f;
+                    auto font_size = orig_font_size / dist_sq;
+                    if (font_size > orig_font_size)
+                        font_size = orig_font_size;
+
+                    auto size = graphics::get_text_width(nameplate_font, player->name) * (1 / dist_sq);
+                    auto render_x = screen.x;
+                    auto render_y = screen.y;
+                    graphics::draw_text(nameplate_font, player->name, screen.x - (size / 2), screen.y, 1 / dist_sq, 0xFFFFFFFF, true);
+                    
+                    constexpr auto health_width = 135.0f;
+                    constexpr auto health_height = 15.0f;
+
+                    auto scaled_width = health_width / dist_sq;
+                    auto scaled_height = health_height / dist_sq;
+
+                    if (scaled_width > health_width)
+                        scaled_width = health_width;
+
+                    if (scaled_height > health_height)
+                        scaled_height = health_height;
+
+                    screen.y += scaled_height + 5.0f;
+                    render_x = screen.x - (scaled_width / 2);
+
+                    zpl_vec3 health_color_min = { 255.0f, 0.0f, 0.0f };
+                    zpl_vec3 health_color_max = { 0.0f, 255.0f, 0.0f };
+                    zpl_vec3 calculated_color, background_color;
+                    f32 health = zpl_clamp(player_health / 200.0f, 0.0f, 1.0f);
+                    zpl_vec3_lerp(&calculated_color, health_color_min, health_color_max, health);
+                    zpl_vec3_mul(&background_color, calculated_color, 0.2f);
+
+                    DWORD final_color = mod_vec3_to_color(calculated_color);
+                    DWORD final_bg_color = mod_vec3_to_color(background_color);
+
+                    float width = (player_health * scaled_width) / 200.0f;
+                    graphics::draw_box(device, render_x - 2.0f, screen.y - 2.0f, scaled_width + 2.0f, scaled_height + 2.0f, final_bg_color);
+                    graphics::draw_box(device, render_x, screen.y, width, scaled_height, final_color);
+                }
+            }
+        });
     }
 }
--- Code/Client/Graphics/base.hpp
+++ Code/Client/Graphics/base.hpp
@@ -3,10 +3,18 @@
 #include "Graphics/d3d9/CDirect3D9Proxy.h"
 #include "utils.hpp"
 
+namespace nameplates {
+    inline void init(IDirect3DDevice9* device);
+    inline void device_lost();
+    inline void device_reset(IDirect3DDevice9* device);
+    inline void render(IDirect3DDevice9* device);
+}
+
 namespace graphics {
     
     typedef IDirect3D9 *(WINAPI * d3dcreate9_t)(UINT);
     d3dcreate9_t d3dcreate9_original = nullptr;
+    ID3DXSprite* main_sprite = nullptr;
     
     IDirect3D9* WINAPI d3dcreate9_hook(UINT SDKVersion) {
         IDirect3D9 *new_direct = d3dcreate9_original(SDKVersion);
@@ -20,12 +28,78 @@ namespace graphics {
     }
 
     inline auto hook() {
+        while (!GetModuleHandle("d3d9.dll")) Sleep(100);
+        d3dcreate9_original = (d3dcreate9_t)(DetourFunction(DetourFindFunction((char*)"d3d9.dll", (char*)"Direct3DCreate9"), (PBYTE)d3dcreate9_hook));
+    }
 
-        while (!GetModuleHandle("d3d9.dll")) {
-            Sleep(100);
+    inline auto create_font(IDirect3DDevice9* device, const char* font_name, unsigned int size, bool bold) -> ID3DXFont* {
+        ID3DXFont* to_create = nullptr;
+
+        if (FAILED(D3DXCreateFont(device, size, 0, (bold ? FW_BOLD : FW_NORMAL), 1, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, (DEFAULT_PITCH | FF_DONTCARE), font_name, &to_create))) {
+            MessageBox(NULL, "Unable to create font", "nameplates.hpp", MB_OK);
+            return nullptr;
         }
 
-        d3dcreate9_original = (d3dcreate9_t)(DetourFunction(DetourFindFunction((char*)"d3d9.dll", (char*)"Direct3DCreate9"), (PBYTE)d3dcreate9_hook));
+        return to_create;
+    }
+
+    inline auto get_text_width(ID3DXFont* font, const char* text) -> int {
+
+        if (font) {
+            HDC dc = font->GetDC();
+            SIZE size;
+            GetTextExtentPoint32(dc, text, strlen(text), &size);
+            return size.cx;
+        }
+        return 0;
+    }
+
+    struct Vertex2D {
+        float x, y, z, rhw;
+        DWORD color;
+    };
+
+    inline void draw_box(IDirect3DDevice9* device, float x, float y, float width, float height, DWORD color) {
+        
+        const Vertex2D rect[] = {
+            { x,			y,			0.0f, 1.0f,	color },
+            { x + width,	y,			0.0f, 1.0f,	color },
+            { x,			y + height, 0.0f, 1.0f,	color },
+            { x + width,	y + height, 0.0f, 1.0f,	color },
+        };
+
+        device->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+        device->SetPixelShader(NULL);
+        device->SetVertexShader(NULL);
+        device->SetRenderState(D3DRS_ZENABLE, FALSE);
+        device->SetTexture(0, NULL);
+        device->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, &rect, sizeof(Vertex2D));
+    }
+
+    /*
+    *  Just draw some text boi
+    */
+    inline void draw_text(ID3DXFont* font, const char* text, float x, float y, float scale, unsigned long color, bool shadow) {
+        if (text == nullptr || main_sprite == nullptr || font == nullptr) return;
+
+        D3DXVECTOR2 scaling(scale, scale);
+        D3DXMATRIX matrix;
+        D3DXMatrixTransformation2D(&matrix, NULL, 0.0f, &scaling, NULL, 0.0f, NULL);
+
+        main_sprite->Begin(D3DXSPRITE_ALPHABLEND | D3DXSPRITE_SORT_TEXTURE);
+        main_sprite->SetTransform(&matrix);
+
+        if (shadow) {
+            RECT shadow_rect;
+            SetRect(&shadow_rect, x * (1.0f / scale) + 1, y * (1.0f / scale) + 1, 0, 0);
+            font->DrawTextA(main_sprite, text, -1, &shadow_rect, DT_NOCLIP, D3DCOLOR_ARGB(255, 0, 0, 0));
+        }
+
+        RECT rect;
+        SetRect(&rect, x * (1.0f / scale), y * (1.0f / scale), 0, 0);
+        font->DrawTextA(main_sprite, text, -1, &rect, DT_NOCLIP, color);
+
+        main_sprite->End();
     }
 
     inline D3DSURFACE_DESC get_backbuffer_desc(IDirect3DDevice9* device) {
@@ -37,20 +111,39 @@ namespace graphics {
         return back_buffer_desc;
     }
 
+    inline auto init_main_sprite(IDirect3DDevice9* device) -> void {
+        if (FAILED(D3DXCreateSprite(device, &main_sprite))) {
+            MessageBox(NULL, "Unable to create sprite for drawing nameplates", "nameplates.hpp", MB_OK);
+            return;
+        }
+    }
+
     inline auto init(IDirect3DDevice9* device) -> void {
+
+        init_main_sprite(device);
         global_device = device;
+        nameplates::init(device);
         effects::init(device);
         cef::init(device);
         chat::init(device);
     }
 
     inline auto device_lost(IDirect3DDevice9* device) -> void {
+
+        if (main_sprite) {
+            main_sprite->Release();
+            main_sprite = nullptr;
+        }
+
+        nameplates::device_lost();
         effects::device_lost();
         cef::device_lost();
     }
 
     inline auto device_reset(IDirect3DDevice9* device) -> void {
         global_device = device;
+        init_main_sprite(device);
+        nameplates::device_reset(device);
         effects::device_reset(device);
         cef::device_reset(device);
     }
@@ -66,27 +159,25 @@ namespace graphics {
         IDirect3DDevice9_SetRenderState(device, D3DRS_CULLMODE, D3DCULL_NONE);
         IDirect3DDevice9_SetRenderState(device, D3DRS_LIGHTING, FALSE);
 
-        /* Enable color modulation by diffuse color */
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_COLOROP, D3DTOP_MODULATE);
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
 
-        /* Enable alpha modulation by diffuse alpha */
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
         IDirect3DDevice9_SetTextureStageState(device, 0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
         IDirect3DDevice9_SetRenderState(device, D3DRS_ALPHABLENDENABLE, TRUE);
 
-        /* Disable second texture stage, since we're done */
         IDirect3DDevice9_SetTextureStageState(device, 1, D3DTSS_COLOROP, D3DTOP_DISABLE);
         IDirect3DDevice9_SetTextureStageState(device, 1, D3DTSS_ALPHAOP, D3DTOP_DISABLE);
         IDirect3DDevice9_SetPixelShader(device, NULL);
 
+        nameplates::render(device);
+        effects::render(device);
         chat::update();
         cef::tick();
         cef::render_browsers();
-        effects::render(device);
-        
+
         pStateBlock->Apply();
         pStateBlock->Release();
     }
