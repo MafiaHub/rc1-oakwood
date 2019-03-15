a38c13b6fd8a89a6c31d988a4b52809c54ae42bb
Dominik Madarász <zaklaus@outlook.com>
2019-03-15 13:56:38 +0100

    Update HTTP library + Make linux server work properly

--- Vendors/http/http.h
+++ Vendors/http/http.h
@@ -445,10 +445,10 @@ http_t* http_post( char const* url, void const* data, size_t size, void* memctx 
     HTTP_SOCKET socket = http_internal_connect( address, port );
     if( socket == HTTP_INVALID_SOCKET ) return NULL;
 
-    #ifndef _WIN32
+    /* #ifndef _WIN32
     int optval=1;
     signal(SIGPIPE, SIG_IGN);
-    #endif
+    #endif */
     
     http_internal_t* internal = http_internal_create( size, memctx );
     internal->socket = socket;
