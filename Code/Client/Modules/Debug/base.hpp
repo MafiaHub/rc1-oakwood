49d2bc35fda31f192d3edf53493092697f5b39a6
Dominik Madarász <zaklaus@outlook.com>
2019-03-16 20:24:15 +0100

    Fix mouse look in debug mode

--- Code/Client/Modules/Debug/base.hpp
+++ Code/Client/Modules/Debug/base.hpp
@@ -4,6 +4,8 @@ namespace debug {
     bool is_debug_enabled = false;
     S_vector debug_cam_pos;
     S_vector debug_cam_dir;
+    int delta_x = 0;
+    int delta_y = 0;
 
     inline void init() {
         
@@ -38,24 +40,16 @@ namespace debug {
     }
 
     constexpr float speed = 5.0f;
-#define RADIAN (180.0f /  3.14159f)
+#define RADIAN (180.0f /  ZPL_PI)
 #define ASIN(x) (asin(x) * RADIAN)
-
-    int delta_x = 0;
-    int delta_y = 0;
 
     inline void game_update(f64 delta_time) {
         
         // Update free camera here
         if (is_debug_enabled) {
-
-       
-
-            if (input::InputState.rmb_down) {
-               
+            if (input::InputState.rmb_down) {               
                 POINT current_pos;
                 GetCursorPos(&current_pos);
-
                 ScreenToClient((HWND)MafiaSDK::GetIGraph()->GetMainHWND(), &current_pos);
 
                 RECT client_rect;
@@ -63,18 +57,22 @@ namespace debug {
                     (HWND)MafiaSDK::GetIGraph()->GetMainHWND(),
                     &client_rect
                 );
+                auto width = client_rect.right - client_rect.left;
+                auto height = client_rect.bottom - client_rect.top;
 
-                delta_x = current_pos.x - client_rect.left + MafiaSDK::GetIGraph()->Scrn_sx() / 2;
-                delta_y = current_pos.y - client_rect.top + MafiaSDK::GetIGraph()->Scrn_sy() / 2;
+                delta_x = current_pos.x - width / 2.0f;
+                delta_y = current_pos.y - height / 2.0f;
 
-               
-
-                SetCursorPos(client_rect.left + MafiaSDK::GetIGraph()->Scrn_sx() / 2, 
-                    client_rect.top + MafiaSDK::GetIGraph()->Scrn_sy() / 2);
+                POINT mpos = {width / 2.0f, height / 2.0f};
+                ClientToScreen((HWND)MafiaSDK::GetIGraph()->GetMainHWND(), &mpos);
+                SetCursorPos(mpos.x, mpos.y);
                 
                 input::InputState.mouse_move_delta = { 0, 0 };
             }
-
+            else {            
+                delta_x = 0;
+                delta_y = 0;
+            }
 
             if (abs(delta_x) > 1) {
 
@@ -90,24 +88,24 @@ namespace debug {
                 debug_cam_dir.y = sin((ASIN(debug_cam_dir.y) - (float)delta_y * 0.1f) / RADIAN);
             }
 
-            if (input::is_key_down(VK_UP)) {
+            if (input::is_key_down(0x57)) { // W
                 debug_cam_pos.x += debug_cam_dir.x * 0.2 * speed;
                 debug_cam_pos.y += debug_cam_dir.y * 0.2 * speed;
                 debug_cam_pos.z += debug_cam_dir.z * 0.2 * speed;
             }
 
-            if (input::is_key_down(VK_DOWN)) {
+            if (input::is_key_down(0x53)) { // S
                 debug_cam_pos.x -= debug_cam_dir.x * 0.2 * speed;
                 debug_cam_pos.y -= debug_cam_dir.y * 0.2 * speed;
                 debug_cam_pos.z -= debug_cam_dir.z * 0.2 * speed;
             }
 
-            if (input::is_key_down(VK_LEFT)) {
+            if (input::is_key_down(0x41)) { // A
                 debug_cam_pos.x -= debug_cam_dir.z * 0.2 * speed;
                 debug_cam_pos.z += debug_cam_dir.x * 0.2 * speed;
             }
 
-            if (input::is_key_down(VK_RIGHT)) {
+            if (input::is_key_down(0x44)) { // D
                 debug_cam_pos.x += debug_cam_dir.z * 0.2 * speed;
                 debug_cam_pos.z -= debug_cam_dir.x * 0.2 * speed;
             }
