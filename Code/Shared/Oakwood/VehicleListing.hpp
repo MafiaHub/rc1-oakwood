75b43a9c39eb732da1674db0f063a57f96782b3a
Dominik Madarász <zaklaus@outlook.com>
2019-04-02 22:31:12 +0200

    Refine vehicle catalogue, add ID remarks

--- Code/Shared/Oakwood/VehicleListing.hpp
+++ Code/Shared/Oakwood/VehicleListing.hpp
@@ -2,148 +2,148 @@
 
 std::vector<std::pair<std::string, std::string>> VehicleCatalogue = {
 
-    {"Bolt Ace Coupe Blue",					"fordtco00.i3d"},
-    {"Bolt Ace Coupe Dark Blue",			"fordtco01.i3d"},
-    {"Bolt Ace Coupe Brown",				"fordtco02.i3d"},
-    {"Bolt Ace Coupe Green",				"fordtco03.i3d"},
-    {"Bolt Ace Coupe Red",					"fordtco04.i3d"},
+    {"Bolt Ace Coupe Blue",					"fordtco00.i3d"}, // 0
+    {"Bolt Ace Coupe Dark Blue",			"fordtco01.i3d"}, 
+    {"Bolt Ace Coupe Brown",				"fordtco02.i3d"}, 
+    {"Bolt Ace Coupe Green",				"fordtco03.i3d"}, 
+    {"Bolt Ace Coupe Red",					"fordtco04.i3d"}, 
 
-    {"Bolt Ace Fordor Blue",				"fordtFor00.i3d"},
+    {"Bolt Ace Fordor Blue",				"fordtFor00.i3d"}, // 5
     {"Bolt Ace Fordor Dark Blue",			"fordtFor01.i3d"},
     {"Bolt Ace Fordor Brown",				"fordtFor02.i3d"},
     {"Bolt Ace Fordor Green",				"fordtFor03.i3d"},
     {"Bolt Ace Fordor Red",					"fordtFor04.i3d"},
 
-    {"Bolt Ace Pickup Blue",				"fordtpi00.i3d"},
-    {"Bolt Ace Pickup Dark Blue",			"fordtpi01.i3d"},
-    {"Bolt Ace Pickup Brown",				"fordtpi02.i3d"},
-    {"Bolt Ace Pickup Green",				"fordtpi03.i3d"},
-    {"Bolt Ace Pickup Red",					"fordtpi04.i3d"},
+    {"Bolt Ace Pickup Blue",				"fordtpi00.i3d"}, // 10
+    {"Bolt Ace Pickup Dark Blue",			"fordtpi01.i3d"}, 
+    {"Bolt Ace Pickup Brown",				"fordtpi02.i3d"}, 
+    {"Bolt Ace Pickup Green",				"fordtpi03.i3d"}, 
+    {"Bolt Ace Pickup Red",					"fordtpi04.i3d"}, 
 
-    {"Bolt Ace Runabout Blue",				"fordtru00.i3d"},
+    {"Bolt Ace Runabout Blue",				"fordtru00.i3d"}, // 15
     {"Bolt Ace Runabout Dark Blue",			"fordtru01.i3d"},
     {"Bolt Ace Runabout Brown",				"fordtru02.i3d"},
     {"Bolt Ace Runabout Green",				"fordtru03.i3d"},
     {"Bolt Ace Runabout Red",				"fordtru04.i3d"},
 
-    {"Bolt Ace Touring Blue",				"fordtto00.i3d"},
+    {"Bolt Ace Touring Blue",				"fordtto00.i3d"}, // 20
     {"Bolt Ace Touring Dark Blue",			"fordtto01.i3d"},
     {"Bolt Ace Touring Brown",				"fordtto02.i3d"},
     {"Bolt Ace Touring Green",				"fordtto03.i3d"},
     {"Bolt Ace Touring Red",				"fordtto04.i3d"},
 
-    {"Bolt Ace Tudor Blue",					"fordttud00.i3d"},
+    {"Bolt Ace Tudor Blue",					"fordttud00.i3d"}, // 25
     {"Bolt Ace Tudor Dark Blue",			"fordttud01.i3d"},
     {"Bolt Ace Tudor Brown",				"fordttud02.i3d"},
     {"Bolt Ace Tudor Green",				"fordttud03.i3d"},
     {"Bolt Ace Tudor Red",					"fordttud04.i3d"},
 
-    {"Bolt Model B Cabriolet Brown",		"ForAca00.i3d"},
+    {"Bolt Model B Cabriolet Brown",		"ForAca00.i3d"}, // 30
     {"Bolt Model B Cabriolet Red",			"ForAca01.i3d"},
     {"Bolt Model B Cabriolet Green",		"ForAca02.i3d"},
     {"Bolt Model B Cabriolet Dark Blue",	"ForAca03.i3d"},
 
-    {"Bolt Model B Coupe Brown",			"ForAcou00.i3d"},
+    {"Bolt Model B Coupe Brown",			"ForAcou00.i3d"}, // 34
     {"Bolt Model B Coupe Red",				"ForAcou01.i3d"},
     {"Bolt Model B Coupe Green",			"ForAcou02.i3d"},
     {"Bolt Model B Coupe Dark Blue",		"ForAcou03.i3d"},
 
-    {"Bolt Model B Delivery Brown",			"ForAde00.i3d"},
+    {"Bolt Model B Delivery Brown",			"ForAde00.i3d"}, // 38
     {"Bolt Model B Delivery Red",			"ForAde01.i3d"},
     {"Bolt Model B Delivery Green",			"ForAde02.i3d"},
     {"Bolt Model B Delivery Dark Blue",		"ForAde03.i3d"},
 
-    {"Bolt Model B Fordor Brown",			"ForAfo00.i3d"},
+    {"Bolt Model B Fordor Brown",			"ForAfo00.i3d"}, // 42
     {"Bolt Model B Fordor Red",				"ForAfo01.i3d"},
     {"Bolt Model B Fordor Green",			"ForAfo02.i3d"},
     {"Bolt Model B Fordor Dark Blue",		"ForAfo03.i3d"},
 
-    {"Bolt Model B Pickup Brown",			"ForApic00.i3d"},
+    {"Bolt Model B Pickup Brown",			"ForApic00.i3d"}, // 46
     {"Bolt Model B Pickup Red",				"ForApic01.i3d"},
     {"Bolt Model B Pickup Green",			"ForApic02.i3d"},
     {"Bolt Model B Pickup Dark Blue",		"ForApic03.i3d"},
 
-    {"Bolt Model B Roadster Brown",			"ForAro00.i3d"},
+    {"Bolt Model B Roadster Brown",			"ForAro00.i3d"}, // 50
     {"Bolt Model B Roadster Red",			"ForAro01.i3d"},
     {"Bolt Model B Roadster Green",			"ForAro02.i3d"},
     {"Bolt Model B Roadster Dark Blue",		"ForAro03.i3d"},
 
-    {"Bolt Model B Tudor Brown",			"ForAtu00.i3d"},
+    {"Bolt Model B Tudor Brown",			"ForAtu00.i3d"}, // 54
     {"Bolt Model B Tudor Red",				"ForAtu01.i3d"},
     {"Bolt Model B Tudor Green",			"ForAtu02.i3d"},
     {"Bolt Model B Tudor Dark Blue",		"ForAtu03.i3d"},
 
-    {"Bolt V8 Coupe Green",					"forVco00.i3d"},
+    {"Bolt V8 Coupe Green",					"forVco00.i3d"}, // 58
     {"Bolt V8 Coupe Red",					"forVco01.i3d"},
     {"Bolt V8 Coupe Blue",					"forVco02.i3d"},
     {"Bolt V8 Coupe Grey",					"forVco03.i3d"},
 
-    {"Bolt V8 Fordor Green",				"forVfor00.i3d"},
+    {"Bolt V8 Fordor Green",				"forVfor00.i3d"}, // 62
     {"Bolt V8 Fordor Red",					"forVfor01.i3d"},
     {"Bolt V8 Fordor Blue",					"forVfor02.i3d"},
     {"Bolt V8 Fordor Grey",					"forVfor03.i3d"},
 
-    {"Bolt V8 Roadster Green",				"forVro00.i3d"},
+    {"Bolt V8 Roadster Green",				"forVro00.i3d"}, // 66
     {"Bolt V8 Roadster Red",				"forVro01.i3d"},
     {"Bolt V8 Roadster Blue",				"forVro02.i3d"},
     {"Bolt V8 Roadster Grey",				"forVro03.i3d"},
 
-    {"Bolt V8 Touring Green",				"forVto00.i3d"},
+    {"Bolt V8 Touring Green",				"forVto00.i3d"}, // 70
     {"Bolt V8 Touring Red",					"forVto01.i3d"},
     {"Bolt V8 Touring Blue",				"forVto02.i3d"},
     {"Bolt V8 Touring Grey",				"forVto03.i3d"},
 
-    {"Bolt V8 Tudor Green",					"forVtud00.i3d"},
+    {"Bolt V8 Tudor Green",					"forVtud00.i3d"}, // 74
     {"Bolt V8 Tudor Red",					"forVtud01.i3d"},
     {"Bolt V8 Tudor Blue",					"forVtud02.i3d"},
     {"Bolt V8 Tudor Grey",					"forVtud03.i3d"},
 
-    {"Brubaker 4WD",						"miller00.i3d"},
+    {"Brubaker 4WD",						"miller00.i3d"}, // 78
 
-    {"Brauno Speedster 851 Silver",			"speedster00.i3d"},
+    {"Brauno Speedster 851 Silver",			"speedster00.i3d"}, // 79
     {"Brauno Speedster 851 Red",			"speedster01.i3d"},
     {"Brauno Speedster 851 Green",			"speedster02.i3d"},
 
-    {"Caesar 8C 2300 Racing",				"alfa00.i3d"},
+    {"Caesar 8C 2300 Racing",				"alfa00.i3d"}, // 82
     {"Caesar 8C Mostro Red",				"alfa8C00.i3d"},
     {"Caesar 8C Mostro Black",				"alfa8C01.i3d"},
 
-    {"Celeste Marque 500 White",			"merced500K00.i3d"},
+    {"Celeste Marque 500 White",			"merced500K00.i3d"}, // 85
     {"Celeste Marque 500 Brown",			"merced500K01.i3d"},
 
-    {"Corrozella C-Otto 4WD Blue",			"bugatti00.i3d"},
+    {"Corrozella C-Otto 4WD Blue",			"bugatti00.i3d"}, // 87
     {"Corrozella C-Otto 4WD Green",			"bugatti01.i3d"},
 
-    {"Crusader Chromim Fordor Blue",		"pontFor00.i3d"},
+    {"Crusader Chromim Fordor Blue",		"pontFor00.i3d"}, // 89
     {"Crusader Chromim Fordor Pink",		"pontFor01.i3d"},
     {"Crusader Chromim Tudor Green",		"pontTud00.i3d"},
     {"Crusader Chromim Tudor Dark Blue",	"pontTud01.i3d"},
 
-    {"Falconer Blue",						"blackha00.i3d"},
+    {"Falconer Blue",						"blackha00.i3d"}, // 93
     {"Falconer Pink",						"blackha01.i3d"},
     {"Falconer Gangster",					"black00.i3d"},
     {"Falconer Yellowcar",					"taxi00.i3d"},
 
-    {"Guardian Terraplane Coupe Pink",		"hudcou00.i3d"},
+    {"Guardian Terraplane Coupe Pink",		"hudcou00.i3d"}, // 97
     {"Guardian Terraplane Coupe Beige",		"hudcou01.i3d"},
     {"Guardian Terraplane Coupe Black",		"hudcou02.i3d"},
 
-    {"Guardian Terraplane Fordor Pink",		"hudfor00.i3d"},
+    {"Guardian Terraplane Fordor Pink",		"hudfor00.i3d"}, // 100
     {"Guardian Terraplane Fordor Beige",	"hudfor01.i3d"},
     {"Guardian Terraplane Fordor Black",	"hudfor02.i3d"},
 
-    {"Guardian Terraplane Tudor Pink",		"hudtu00.i3d"},
+    {"Guardian Terraplane Tudor Pink",		"hudtu00.i3d"}, // 103
     {"Guardian Terraplane Tudor Beige",		"hudtu01.i3d"},
     {"Guardian Terraplane Tudor Black",		"hudtu02.i3d"},
 
-    {"Lassister V16 Appolon",				"hartmann00.i3d"},
+    {"Lassister V16 Appolon",				"hartmann00.i3d"}, // 106
     {"Lassister V16 Charron",				"hearseCa00.i3d"},
     {"Lassister V16 Fordor",				"cad_ford00.i3d"},
     {"Lassister V16 Phaeton",				"cad_phaeton00.i3d"},
     {"Lassister V16 Police",				"polCad00.i3d"},
     {"Lassister V16 Roadster",				"cad_road00.i3d"},
 
-    {"Schubert Extra Six Fordor Green",		"chemaFor00.i3d"},
+    {"Schubert Extra Six Fordor Green",		"chemaFor00.i3d"}, // 112
     {"Schubert Extra Six Fordor White",		"chemaFor01.i3d"},
     {"Schubert Extra Six Fordor Blue",		"chemaFor02.i3d"},
     {"Schubert Extra Six Fordor Police",	"polimFor00.i3d"},
@@ -156,40 +156,38 @@ std::vector<std::pair<std::string, std::string>> VehicleCatalogue = {
     {"Schubert Six Black",					"chev02.i3d"},
     {"Schubert Six Police",					"poli00.i3d"},
 
-    {"Silver Fletcher",						"arrow00.i3d"},
+    {"Silver Fletcher",						"arrow00.i3d"}, // 124
 
-    {"Thor 810 Phaeton FWD Orange",			"cordph00.i3d"},
+    {"Thor 810 Phaeton FWD Orange",			"cordph00.i3d"}, // 125
     {"Thor 810 Phaeton FWD Black",			"cordph01.i3d"},
     {"Thor 810 Sedan FWD Orange",			"cordse00.i3d"},
     {"Thor 810 Sedan FWD Black",			"cordse01.i3d"},
     {"Thor 812 Cabriolet FWD Orange",		"cordca00.i3d"},
     {"Thor 812 Cabriolet FWD Black",		"cordca01.i3d"},
 
-    {"Trautenberg Model J",					"deuseJco00.i3d"},
+    {"Trautenberg Model J",					"deuseJco00.i3d"}, // 132
     {"Trautenberg Racer 4WD",				"duesenberg00.i3d"},
 
-    {"Ulver Airstream Fordor Yellow",		"airflFor00.i3d"},
+    {"Ulver Airstream Fordor Yellow",		"airflFor00.i3d"}, // 134
     {"Ulver Airstream Fordor Green",		"airflFor01.i3d"},
     {"Ulver Airstream Tudor Yellow",		"airfltud00.i3d"},
     {"Ulver Airstream Tudor Green",			"airfltud01.i3d"},
 
-    {"Wright Coupe Blue",					"buiCou00.i3d"},
+    {"Wright Coupe Blue",					"buiCou00.i3d"}, // 138
     {"Wright Coupe Green",					"buiCou01.i3d"},
     {"Wright Coupe Gangster",				"buigang00.i3d"},
     {"Wright Fordor Blue",					"buikFor00.i3d"},
     {"Wright Fordor Green",					"buikFor01.i3d"},
 
-    {"Bolt Ambulance",						"Ambulance00.i3d"},
+    {"Bolt Ambulance",						"Ambulance00.i3d"}, // 143
     {"Bolt Firetruck",						"fire00.i3d"},
     {"Bolt Hearse",							"hearseA00.i3d"},
     {"Bolt Truck Flatbed",					"truckA00.i3d"},
     {"Bolt Truck Covered",					"truckB00.i3d"},
     {"Bolt Truck",							"truckBx00.i3d"},
-    {"Manta Prototype",						"phantom00.i3d"}
+    {"Manta Prototype",						"phantom00.i3d"},
 
-/* These vehicles are disabled because they are either prototypes or simply unfinished.
-    Some of these vehicles can cause sync issues, since they weren't primarily made for the physics system the game has.
-    {"Black Dragon 4WD",					"blackdragon00.i3d"},
+    {"Black Dragon 4WD",					"blackdragon00.i3d"}, // 150
     {"Black Metal 4WD",						"chevroletm6H00.i3d"},
     {"Bob Mylan 4WD",						"hotrodp200.i3d"},
     {"Bolt-Thrower",						"Thunderbird00.i3d"},
@@ -208,6 +206,9 @@ std::vector<std::pair<std::string, std::string>> VehicleCatalogue = {
     {"Mutagen FWD",							"cord_sedanH00.i3d"},
     {"Speedee 4WD",							"hotrodp400.i3d"},
 
+/* These vehicles are disabled because they are simply unfinished or testing vehicles.
+    Some of these vehicles can cause sync issues, since they weren't primarily made for the physics system the game has.
+    
     {"Beatle Custom",						"brouk00.i3d"},
     {"Bulldozer",							"Bull00.i3d"},
     {"Bus",									"bus00.i3d"},
