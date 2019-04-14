5fd0810459d6647ac4d2b7a36c905bdc7fc914e1
DavoSK <david.svitana@gmail.com>
2019-04-14 19:23:45 +0200

    Dont show playlist while not connected

--- Code/Client/Modules/Playerlist/base.hpp
+++ Code/Client/Modules/Playerlist/base.hpp
@@ -2,7 +2,8 @@ namespace playerlist {
     constexpr int VKEY_P = 0x50;
 
     void render() {
-        if (!input::is_key_down(VKEY_P) || modules::chat::is_focused) 
+        if (!input::is_key_down(VKEY_P) || modules::chat::is_focused || 
+            !librg_is_connected(&network_context)) 
             return;
 
         ImGui::SetNextWindowPosCenter();
