581a50d57710e397a0a34e8c95fb772fd217dbe0
Dominik Madarász <zaklaus@outlook.com>
2019-06-28 15:13:43 +0200

    Freeride mod

---
+++ Code/Plugins/Freeride/premake5.lua
@@ -0,0 +1,25 @@
+project "Freeride"
+    language "C++"
+    kind "SharedLib"
+    targetdir "../../../Bin/%{cfg.buildcfg}/plugins/"
+    vpaths { ["*"] = "*" }
+    files {
+        "premake5.lua",
+	    "../../Shared/Oakwood/*.cpp",
+        "../../Shared/Oakwood/*.hpp",    
+        "**.h",
+        "**.hpp",
+        "**.cpp",
+        "**.rc"
+    }
+    includedirs {
+        "../../Shared",
+        "../../../Vendors/librg",
+        "../../../Vendors"
+    }
+    configuration "linux or macosx"
+        links {
+            "pthread",
+            "dl"
+        }
+
