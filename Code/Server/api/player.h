b998eec5555246f752605b88e99e940d57738c21
Dominik Madarász <zaklaus@outlook.com>
2019-08-05 12:12:17 +0200

    Additional changes

--- Code/Server/api/player.h
+++ Code/Server/api/player.h
@@ -358,8 +358,8 @@ int oak_player_visibility_get(oak_player id, oak_visiblity_type type) {
 
 /**
  * Get online players
- * @param  count [description]
- * @return       [description]
+ * @param  count
+ * @return
  */
 oak_player **oak_player_list(int *count) {
     return (oak_player **)oak_entity_list(OAK_PLAYER, count);
