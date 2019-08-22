343ed3fcd5ac59f4640dad024bfffbafd2d0cbab
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-08-22 21:36:07 +0300

    added temp weapon spawning

--- Code/Server/api/player.h
+++ Code/Server/api/player.h
@@ -1,3 +1,44 @@
+/* TEMP WEAPON STUFF */
+struct tmp_weapon {
+    const char *model;
+    inventory_item item;
+};
+
+tmp_weapon colt = { "2c1911.i3d",{ 9, 50, 50, 0 } };
+tmp_weapon tommy = { "2tommy.i3d",{ 10, 50, 50, 0 } };
+tmp_weapon grenade = { "2grenade.i3d",{ 15, 1, 1, 0 } };
+//weapon palka = { "2palka.i3d",{ 4, 1, 1, 0 } };
+
+tmp_weapon *tmp_weaponlist[] = {
+    &tommy,
+    &colt,
+    &grenade
+  //  &palka
+};
+
+auto TEMP_add_weapons(oak_player id) {
+    if (oak_player_invalid(id)) return -1;
+    auto player = oak_entity_player_get(id);
+
+    for (int g = 0; g < zpl_count_of(tmp_weaponlist); g++) {
+        auto gun = &tmp_weaponlist[g]->item;
+
+        //serverside insert weapon into free slot
+        for (size_t i = 0; i < 8; i++)
+        {
+            auto cur_item = player->inventory.items[i];
+            if (cur_item.weaponId == -1)
+            {
+                player->inventory.items[i] = *gun;
+                break;
+            }
+        }
+
+        //set new weapon as current
+        player->current_weapon_id = gun->weaponId;
+    }
+}
+
 // =======================================================================//
 // !
 // ! General
@@ -74,6 +115,8 @@ int oak_player_spawn(oak_player id) {
         librg_data_wu32(&data, player->current_weapon_id);
         librg_data_wf32(&data, player->health);
     });
+
+    TEMP_add_weapons(id);
 
     return 0;
 }
