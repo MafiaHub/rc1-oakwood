66fdf7f4ac63fc2991bdfbb2cbfda981fa4d5e84
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-08-29 17:14:14 +0300

    added davo's racing mode

--- Resources/racing/index.js
+++ Resources/racing/index.js
@@ -1,7 +1,26 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random() * this.length))];
+}
+
 const fs = require('fs')
-const EventEmitter = require('events')
-const {createClient, constants} = require('oakwood')
-const {vehicleModels, playerModels} = require('oakwood')
+const { createClient, constants } = require('oakwood')
+const { vehicleModels, playerModels } = require('oakwood')
+
+const oak = createClient(process.platform !== "win32" ? {
+    inbound: 'tcp://192.168.1.3:10101',
+    outbound: 'tcp://192.168.1.3:10102',
+} : {})
+
+/*
+* Gamemode constants
+*/
+const PLAYER_STATE_RACING = 'PLAYER_STATE_RACING',
+      PLAYER_STATE_WAITING = 'PLAYER_STATE_WAITING'
+
+const RACE_STARTING = 'RACE_STARTING',
+      RACE_WARMUP = 'RACE_WARMUP',
+      RACE_ACTIVE = 'RACE_ACTIVE',
+      RACE_FINISHED = 'RACE_FINISHED'
 
 const {
     VISIBILITY_NAME,
@@ -11,269 +30,254 @@ const {
     VISIBILITY_COLLISION,
 } = constants
 
-const oak = createClient(process.platform !== "win32" ? {
-    inbound: 'tcp://192.168.1.3:10101',
-    outbound: 'tcp://192.168.1.3:10102',
-} : {})
+const vehicleModelsList = [
+    87, 88
+]
 
-/* get random element from an array */
-const rndarr = arr => arr[Math.floor(Math.random()*arr.length)]
-const delay = amt => new Promise(r => setTimeout(r, amt))
+const vehicleSpawnPositions = [
+    [305.79599, 0, -137.582474],
+    [297.17865, 0, -148.994385],
+    [288.64566, 0, -137.530869],
+    [280.282654, 0, -148.832489],
+    [271.688232, 0, -137.026657],
+    [263.18042, 0, -148.353851],
+    [254.687592, 0, -136.756195],
+    [246.128525, 0, -147.928497],
+    [237.085098, 0, -136.462891],
+    [228.723404, 0, -147.91481],
+    [220.130203, 0, -136.46846],
+    [211.618027, 0, -147.823135],
+    [203.104935, 0, -135.873718],
+    [194.651489, 0, -147.212463],
+    [186.116394, 0, -135.614044],
+    [177.67836, 0, -146.946365]
+]
 
-function shuffle(array) {
-    let counter = array.length;
+/* 
+* Global variables
+*/
+let mainTicker = null
+let raceState = RACE_FINISHED
+let raceParticipants = {}
+let raceCheckpoints = []
+let raceCheckStateInteval = new Date()
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+/*
+* check race state only in 5 second interval
+*/
+const onRaceTick = () => {
+    if(new Date() - raceCheckStateInteval > 5000) {
+        raceCheckState()
+        raceCheckStateInteval = new Date()
     }
-
-    return array;
 }
 
-oak.event('start', () => {
-    console.log('[info] connection started')
-    oak.log('[info] oakwood-node connected')
-})
+/* 
+* restart player states
+*/
+const raceFinished = async () => {
+    Object.values(raceParticipants).map(async player => {
+        await oak.playerDespawn(player.id)
 
-oak.event('stop', () => {
-    console.log('[info] connection stopped')
-})
+        if(player.vehicle > -1)
+            await oak.vehicleDespawn(player.vehicle)
 
-oak.event('playerConnect', pid => {
+        player.state = PLAYER_STATE_WAITING
+
+        setupStartingCamera(player.id)
+
+        oak.hudMessage(player.id, 'Race finished, wating for enough players...', 0xFF0000)
+    })
+
+}
+
+const raceCheckState = async () => {
+    const participantsArray = Object.values(raceParticipants)
+
+    // console.log(raceState, participantsArray.filter(player => player.laps === raceCheckpoints.length).length, participantsArray.filter(player => player.state !== PLAYER_STATE_RACING).length)
+
+    //when some player finished racing circuit or 
+    //player count in race has lowered and race cannot continue
+
+    const activeRacers = participantsArray.filter(player => player.state === PLAYER_STATE_RACING)
+    if (raceState === RACE_ACTIVE && activeRacers.length < 2) {
+        console.log('FINSIHING RACE')
+
+        raceFinished()
+        raceState = RACE_FINISHED
+    }
+
+    if (raceState === RACE_FINISHED) {
+        raceState = RACE_STARTING
+    }
+
+    //NOTE(DavoSK): check if race is in starting mode and there are more then 1 player waiting 
+    if (raceState === RACE_STARTING && 
+        participantsArray.filter(player => player.state === PLAYER_STATE_WAITING).length > 1) {
+
+        //NOTE(DavoSK): pepare vehicles on start & change race state to starting 
+        raceState = RACE_WARMUP
+
+        let playerCounter = 0
+
+        Object.values(raceParticipants).map(async player => {
+            const spawnPos = vehicleSpawnPositions[playerCounter++]
+            const playerPos = [].concat(spawnPos)
+
+            player.state            = PLAYER_STATE_RACING
+            player.laps             = 0
+            player.lastCheckpoint   = 0
+            player.vehicle          = await oak.vehicleSpawn(vehicleModels[vehicleModelsList.random()][1], spawnPos, 90)
+
+            playerPos[2] += 1.5;
+
+            await oak.playerPositionSet(player.id, playerPos)
+            await oak.playerHealthSet(player.id, 200.0)
+            await oak.playerSpawn(player.id)
+
+                oak.vehiclePlayerPut(player.vehicle, player.id, 0)
+            // oak.cameraUnlock(player.id)
+
+            setTimeout(() => {
+            }, 1000)
+        })
+
+        let currentCountdown = 3
+        const countDownInterval = setInterval(() => {
+            Object.values(raceParticipants).map(player => {
+                if (player && player.state === PLAYER_STATE_RACING) {
+                    oak.hudCountdown(player.id, currentCountdown)
+                }
+            })
+
+            if (--currentCountdown < 0) {
+                clearInterval(countDownInterval)
+                raceState = RACE_ACTIVE
+            }
+        }, 1000)
+    }
+}
+
+const raceStart = async () => {
+
+    //#1 clean old vehicles and ticker
+    if (mainTicker)
+        clearInterval(mainTicker)
+
+    raceState = RACE_STARTING
+    cleanClientState()
+
+    //#2 get checkpoints from file
+    raceCheckpoints = fs.readFileSync('route.txt', 'utf8')
+                        .split('\n')
+                        .map(l => l.split(','))
+
+    mainTicker = setInterval(onRaceTick, 100)
+}
+
+const cleanClientState = async () => {
+    for(vid of await oak.vehicleList())
+        oak.vehicleDespawn(vid)
+
+    for (pid of await oak.playerList())
+        oak.playerDespawn(pid)
+}
+
+const setupStartingCamera = (pid) => {
     const camPos = [316.769, 12.445, -166.209]
     const camDir = [-0.915, -0.346, 0.402]
 
-    oak.cameraSet(pid, camPos, camDir)
-    setTimeout(() => oak.cameraSet(pid, camPos, camDir), 250)
-})
-
-/* Gamemode */
-
-oak.event('start', async () => {
-    /* despawn all empty vehicles */
-    const existing = await oak.vehicleList()
-    if (existing.length > 0) {
-        for (var i = 0; i < existing.length; i++) oak.vehicleDespawn(existing[i])
-    }
-})
-
-const distance = (a, b) => (Math.pow(a[0] - b[0], 2) + Math.pow(a[2] - b[2], 2))
-
-class Race extends EventEmitter {
-    constructor() {
-        super()
-
-        this.state = 'waiting'
-        this.route = fs
-            .readFileSync('route.txt', 'utf8')
-            .split('\n')
-            .map(l => l.split(','))
-
-        this.positions = [
-            [305.79599, 0, -137.582474],
-            [297.17865, 0, -148.994385],
-            [288.64566, 0, -137.530869],
-            [280.282654, 0, -148.832489],
-            [271.688232, 0, -137.026657],
-            [263.18042, 0, -148.353851],
-            [254.687592, 0, -136.756195],
-            [246.128525, 0, -147.928497],
-            [237.085098, 0, -136.462891],
-            [228.723404, 0, -147.91481],
-            [220.130203, 0, -136.46846],
-            [211.618027, 0, -147.823135],
-            [203.104935, 0, -135.873718],
-            [194.651489, 0, -147.212463],
-            [186.116394, 0, -135.614044],
-            [177.67836, 0, -146.946365],
-        ]
-
-        this.participants = []
-        this.vehicles = []
-        this.proute = {}
-
-        this.ticker = null
-    }
-
-    async start() {
-        console.log('starting race')
-
-        this.ticker = setInterval(this.tick.bind(this), 100)
-
-        for (var i = 0; i < this.participants.length; i++) {
-            const pid = this.participants[i]
-            const veh = await oak.vehicleSpawn("bugatti00.i3d", this.positions[i], 90.0)
-
-            oak.vehiclePlayerPut(veh, pid, 0)
-            this.vehicles.push(veh)
-        }
-
-
-        this.state = 'race'
-    }
-
-    stop() {
-        clearInterval(this.ticker)
-    }
-
-    async tick() {
-        if (this.state === 'race') {
-            this.participants.map(async pid => {
-                const pos = await oak.playerPositionGet(pid)
-                const rot1 = this.proute[pid][0]
-                const rot2 = this.proute[pid][1]
-                const diff = distance(pos, rot1)
-
-                if (diff < Math.pow(12, 2)) {
-                    this.proute[pid].shift()
-                    this.emit('checkpoint', pid)
-                }
-
-                /* skipped checkpoint check */
-                if (!rot2) return
-                const diff2 = distance(pos, rot2)
-
-                if (diff2 < Math.pow(12, 2)) {
-                    this.emit('wrongWay', pid)
-                }
-            })
-        }
-    }
-
-    participantAdd(pid) {
-        if (this.state !== 'waiting')
-            return false;
-
-        this.participants.push(pid)
-        this.proute[pid] = [].concat(this.route)
-
-        return true;
-    }
-
-    participantRemove(pid, spectate = true) {
-        this.participants = this.participants
-            .filter(aid => aid !== pid)
-
-        // TODO
-    }
+    // setTimeout(() => oak.cameraSet(pid, camPos, camDir), 250)
 }
 
-const race = new Race();
+/*
+* on node start
+*/
+oak.event('start', async () => {
+    console.log('[info] connection started')
+    oak.log('[info] oakwood-node connected')
 
-race.on('start', async (pid) => {
+    raceStart()
 
+    /* get previously connected players */
+    const ids = await oak.playerList()
+    ids.map(pid => oak.__events['playerConnect'].map(fn => fn(pid)))
 })
 
-race.on('wrongWay', pid => {
-    oak.chatSend(pid, 'wrong way!!!!!!!')
+/* 
+* on node stop
+*/
+oak.event('stop', () => {
+    console.log('[info] connection stopped')
+
+    if (mainTicker)
+        clearInterval(mainTicker)
 })
 
-race.on('checkpoint', async (pid) => {
-    oak.chatSend(pid, 'checkpoint!')
+/* 
+* on player join we announce message about new player 
+* we also tracks player state as new object
+*/
+oak.event('playerConnect', pid => {
+
+    setupStartingCamera(pid)
+    raceParticipants[pid] = {
+        id: pid,
+        laps: 0,
+        lastCheckpoint: 0,
+        vehicle: -1,
+        state: PLAYER_STATE_WAITING
+    }
+
+    oak.hudMessage(pid, 'Welcome to Oakwood racing', 0xFFFFFF)
+    oak.hudMessage(pid, 'Currently youve been added to waiting queue...', 0xFFFFFF)
+    oak.hudMessage(pid, 'Race will start when there will be more then 2 ready players', 0xFFFFFF)
 })
 
-race.on('finish', async (pid) => {
+/* 
+* after player die we change hes state to waiting 
+* so he can spectate other players during race
+*/
+oak.event('playerDeath', async pid => {
+    let playerObject = raceParticipants[pid]
+    if (playerObject) {
+        if(playerObject.vehicle > -1)
+            await oak.vehicleDespawn(playerObject.vehicle)
 
+        await oak.playerDespawn(pid)
+        playerObject.state = PLAYER_STATE_WAITING
+        setupStartingCamera(pid)
+    }
+
+    oak.hudMessage(pid, 'You are dead, now you can spectate others players during race', 0xFF0000)
+
+    for (let otherPid of await oak.playerList())
+        oak.hudMessage(otherPid, 'Player ' + await oak.playerNameGet(pid) + ' died', 0xFFFFFF)
 })
 
-oak.cmd('s', pid => race.start())
+/* 
+* on player diconnect we remove player object from participants
+* and announce disconnection
+*/
+oak.event('playerDisconnect', async pid => {
 
-oak.cmd('j', pid => {
-    race.participantAdd(pid)
+    let playerObject = raceParticipants[pid]
+    if (playerObject && playerObject.vehicle > -1)
+        await oak.vehicleDespawn(playerObject.vehicle)
+
+    delete raceParticipants[pid]
+
+    for(let otherPid of await oak.playerList()) 
+        oak.hudMessage(otherPid, 'Player ' + await oak.playerNameGet(pid) + ' has disconnected', 0xFF0000)
 })
 
-oak.cmd('l', pid => {
-    race.participantRemove(pid)
+oak.cmd('die', pid => {
+    oak.playerKill(pid)
 })
 
-oak.event('playerDeath', pid => {
-    race.participantRemove(pid)
+/*
+* gamemode commands
+*/
+oak.cmd('start', pid => {
+    raceStart()
 })
-
-oak.event('playerDisconnect', pid => {
-    race.participantRemove(pid, false)
-})
-
-
-
-
-
-
-oak.cmd('spawn', async pid => {
-    oak.playerHealthSet(pid, 200)
-    oak.playerPositionSet(pid, race.positions[0])
-    oak.playerSpawn(pid)
-})
-
-// const startRace = async () => {
-//     match.started = true;
-//     shuffle(match.drivers)
-
-//     for (var i = 0; i < match.drivers.length; i++) {
-//         const pid = match.drivers[i]
-//         const veh = await oak.vehicleSpawn("bugatti00.i3d", positions[i], 90.0)
-
-//         match.playerRoutes[pid] = [].concat(match.originalRoute)
-
-//         oak.vehiclePlayerPut(veh, pid, 0)
-//         match.vehicles.push(veh)
-//     }
-
-//     let checker = setInterval(async () => {
-//         const players = await oak.playerList()
-
-//         for (var i = 0; i < players.length; i++) {
-//             // const pid = players[i]
-//             // const pos = await oak.playerPositionGet(pid)
-//             // const route = match.playerRoutes[pid][0]
-
-//             // let dist2 = Math.pow(pos[0] - route[0], 2) + Math.pow(pos[2] - route[2], 2)
-//             // if (dist2 < Math.pow(15, 2)) {
-//             //     match.playerRoutes[pid].shift()
-//             // }
-//         }
-//     }, 100)
-
-
-// }
-
-// /* main timer */
-// setInterval(async () => {
-//     if (!oak.playerList) return;
-//     const players = await oak.playerList()
-
-//     if (!match.started) {
-//         for (var i = 0; i < players.length; i++) {
-//             const pid = players[i]
-//             const diff = Math.round((match.next - Date.now()) / 1000, 1)
-
-//             if (diff != 0) {
-//                 oak.hudAnnounce(pid, `Next race starts in: ${diff}sec`, 2)
-//             }
-
-//             if (diff < 4) {
-//                 oak.hudCountdown(pid, diff)
-//             }
-//         }
-
-//         if (Date.now() > (match.next + 150)) {
-//             startRace()
-//         }
-//     } else {
-//         for (var i = 0; i < players.length; i++) {
-//             const pid = players[i]
-//             oak.hudAnnounce(pid, `Position: ${getRacePosition(pid)}`, 10)
-//         }
-//     }
-// }, 1000)
-
