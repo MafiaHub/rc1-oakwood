dff07ae57e375b5ed57dcf296366c089b5e1dd94
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-09-17 23:26:47 +0300

    fix

--- .github/actions/discord-notification/index.js
+++ .github/actions/discord-notification/index.js
@@ -1,22 +1,26 @@
 const core = require('@actions/core')
 const fetch = require('node-fetch')
 
-try {
-    const platform  = core.getInput('platform')
-    const succeeded = core.getInput('succeeded')
+async function main() {
+    try {
+        const platform  = core.getInput('platform')
+        const succeeded = core.getInput('succeeded')
 
-    const content = succeeded
-        ? `**oakwood** ${platform} build has succeeded :white_check_mark:`
-        : `**oakwood** ${platform} build has failed :no_entry:`
+        const content = succeeded
+            ? `**oakwood** ${platform} build has succeeded :white_check_mark:`
+            : `**oakwood** ${platform} build has failed :no_entry:`
 
-    fetch(`https://discordapp.com/api/webhooks/${process.env.DISCORD_WEBHOOK_CHANNEL_ID}/${process.env.DISCORD_WEBHOOK_TOKEN}`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'user-agent': 'bot'
-        },
-        body: JSON.stringify({ content })
-    })
-} catch (error) {
-    core.setFailed(error.message);
+        await fetch(`https://discordapp.com/api/webhooks/${process.env.DISCORD_WEBHOOK_CHANNEL}/${process.env.DISCORD_WEBHOOK_TOKEN}`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'user-agent': 'bot'
+            },
+            body: JSON.stringify({ content })
+        })
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+main()
