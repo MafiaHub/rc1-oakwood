a57c73b558de586328b057f5c678115cc4f445bf
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-09-18 15:32:49 +0300

    added version.h

---
+++ Code/Shared/version.h
@@ -0,0 +1,15 @@
+#ifndef OAK_VERSION_H
+#define OAK_VERSION_H
+
+#include "semver.h"
+static semver_t oak__version = {0};
+
+#define OAK_NAME "Oakwood"
+#define OAK_VERSION "1.0.0"
+
+#define OAK_VERSION_SEMVER oak__version
+#define OAK_VERSION_MAJOR oak__version.major
+#define OAK_VERSION_MINOR oak__version.minor
+#define OAK_VERSION_PATCH oak__version.patch
+
+#endif // OAK_VERSION_H
---
+++ Scripts/release-bump.js
@@ -0,0 +1,42 @@
+const fs        = require('fs')
+const path      = require('path')
+const {Plugin}  = require('release-it')
+
+const filename  = path.join(__dirname, '..', 'Code', 'Shared', 'version.h')
+
+class Bumper extends Plugin {
+    getLatestVersion() {
+        const data = fs
+            .readFileSync(filename, 'utf8')
+            .trim()
+            .split('\n')
+            .filter(l => l.indexOf('#define OAK_VERSION ') !== -1)
+
+        if (data.length < 1)
+            return null
+
+        return data[0]
+            .replace('#define OAK_VERSION', '')
+            .replace(/\"/, '')
+            .trim()
+    }
+
+    bump(version) {
+        this.version = version;
+
+        const data = fs
+            .readFileSync(filename, 'utf8')
+            .split('\n')
+            .map(l => {
+                if (l.indexOf('#define OAK_VERSION ') !== -1) {
+                    return `#define OAK_VERSION "${version}"`
+                }
+
+                return l
+            })
+
+        fs.writeFileSync(filename, data.join('\n'))
+    }
+}
+
+module.exports = Bumper
