bc9281e085ee53cd2aa731a798406cac20bb941a
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-09-18 22:49:33 +0300

    Release 1.0.19

--- Code/Shared/version.h
+++ Code/Shared/version.h
@@ -5,7 +5,7 @@
 static semver_t oak__version = {0};
 
 #define OAK_NAME "Oakwood"
-#define OAK_VERSION "1.0.18"
+#define OAK_VERSION "1.0.19"
 
 #define OAK_VERSION_SEMVER oak__version
 #define OAK_VERSION_MAJOR oak__version.major
