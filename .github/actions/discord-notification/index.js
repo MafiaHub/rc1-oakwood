67dc106e64503a284b1fbd2773577f2366017fff
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-09-19 19:52:05 +0300

    ci: added discord release hook

--- .github/actions/discord-notification/index.js
+++ .github/actions/discord-notification/index.js
@@ -3,12 +3,20 @@ const fetch = require('node-fetch')
 
 async function main() {
     try {
-        const platform  = core.getInput('platform')
-        const succeeded = core.getInput('succeeded')
+        let type = core.getInput('type')
+        let content = ''
 
-        const content = succeeded == 'true'
-            ? `**oakwood** ${platform} build has succeeded :white_check_mark:`
-            : `**oakwood** ${platform} build has failed :no_entry:`
+        if (type == 'push') {
+            const platform  = core.getInput('platform')
+            const succeeded = core.getInput('succeeded')
+
+            content = succeeded == 'true'
+                ? `**oakwood** ${platform} build has succeeded :white_check_mark:`
+                : `**oakwood** ${platform} build has failed :no_entry:`
+        }
+        else if (type == 'release') {
+            content = `A new **oakwood** release has been deployed :white_check_mark:\nhttps://releases.mafiahub.dev/mafiahub/oakwood/latest`
+        }
 
         await fetch(`https://discordapp.com/api/webhooks/${process.env.DISCORD_WEBHOOK_CHANNEL}/${process.env.DISCORD_WEBHOOK_TOKEN}`, {
             method: 'POST',
